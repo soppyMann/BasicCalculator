594fd5366c00c8999a044aaa97823b0f4b1edd2f
soppyMann <65861874+soppyMann@users.noreply.github.com>
2020-11-21 09:54:11 +0000

    Division result is now an integer

--- main.py
+++ main.py
@@ -2,53 +2,55 @@ operators = ["+", "-", "*", "/", "^"]
 result = 0
 
 
+
+
 while True:
     try:
         num1 = int(input("Type in a number:"))
     except:
-        print("That is not a number!")
+        print("That is not a whole number!")
     else:
         break
+
 
 while True:
     try:
         num2 = int(input("Now type in another number:"))
     except:
-        print("That is not a number!")
+        print("That is not a whole number!")
     else:
         break
 
 
 while True:
-    sym = input("Now type in an operator(+, *, ^, / or -): ")
-    if sym not in operators:
+    operator = input("Now type in an operator(+, *, ^, / or -): ")
+    if operator not in operators:
         print("That is not a recognized operator!")
-    elif num2 == int(0) and sym in "/":
+    elif num2 == int(0) and operator in "/":
         print("You cannot divide by zero!")
         continue
     else:
         break
-    
 
 
 while result == 0:
-    if sym == operators[0]:
+    if operator == operators[0]:
         result = num1 + num2
         print("The result is:", result)
         break
-    elif sym == operators[1]:
+    elif operator == operators[1]:
         result = num1 - num2
         print("The result is:", result)
         break
-    elif sym == operators[2]:
+    elif operator == operators[2]:
         result = num1 * num2
         print("The result is:", result)
         break
-    elif sym == operators[3]:
+    elif operator == operators[3]:
         result = num1 / num2
-        print("The result is:", result)
+        print("The result is:", int(result))
         break
-    elif sym == operators[4]:
+    elif operator == operators[4]:
         result = num1 ** num2
         print("The result is:", result)
         break
