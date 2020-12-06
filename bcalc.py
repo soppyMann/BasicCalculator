9681dbae4f3b97835795a51b00304f3d14d0bd42
soppyMann <65861874+soppyMann@users.noreply.github.com>
2020-12-06 10:03:02 +0000

    Code reduced to just 10 lines

--- bcalc.py
+++ bcalc.py
@@ -4,64 +4,7 @@
 """BasicCalculator is a terminal calculator"""
 
 __author__ = "CBTMaster420 also known as soppyMann"
-__version__ = "0.1"
+__version__ = "0.2"
 
-operators = ["+", "-", "*", "/", "^"]
-result = 0
-
-
-
-
-while True:
-    try:
-        num1 = int(input("Type in a number:"))
-    except:
-        print("That is not a whole number!")
-    else:
-        break
-
-
-while True:
-    try:
-        num2 = int(input("Now type in another number:"))
-    except:
-        print("That is not a whole number!")
-    else:
-        break
-
-
-while True:
-    operator = input("Now type in an operator(+, *, ^, / or -): ")
-    if operator not in operators:
-        print("That is not a recognized operator!")
-    elif num2 == int(0) and operator in "/":
-        print("You cannot divide by zero!")
-        continue
-    else:
-        break
-
-
-while result == 0:
-    if operator == operators[0]:
-        result = num1 + num2
-        print("The result is:", result)
-        break
-    elif operator == operators[1]:
-        result = num1 - num2
-        print("The result is:", result)
-        break
-    elif operator == operators[2]:
-        result = num1 * num2
-        print("The result is:", result)
-        break
-    elif operator == operators[3]:
-        result = num1 / num2
-        if result.is_integer():
-            print("The result is:", int(result))
-        else:
-            print("The result is:", result)
-        break
-    elif operator == operators[4]:
-        result = num1 ** num2
-        print("The result is:", result)
-        break
+calculation = input("Type a calculation:\n")
+print("The answer is: " + str(eval(calculation)))
